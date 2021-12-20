6894c0a7708f239a2b08728168d81673021c024a
Oleg <65137460+jabjik04git@users.noreply.github.com>
2021-12-20 20:40:39 +0300

    Create countingCard.js

---
+++ countingCard.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  let hold = ' Hold';
+   let bet = ' Bet';
+switch(card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+   count++;
+  break;
+  case 7:
+  case 8:
+  case 9:
+   count == count;
+  break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+   count--;
+  break;
+}
+  if (count <= 0) {
+    return count + hold;
+  } else {
+    return count + bet;
+  }
+}
